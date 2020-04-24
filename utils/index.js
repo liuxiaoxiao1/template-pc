2faa27a2df4d62b3c45bc146483c4ff3aea819db
hndzlx@163.com <Admin@123>
2020-04-24 12:51:32 +0800

    添加utils 文件

---
+++ utils/index.js
@@ -0,0 +1,40 @@
+const spawn = require('child_process').spawn  // 一个node的子进程
+
+/**
+ * 安装依赖
+ */
+exports.installDependencies = function installDependencies(
+  cwd,
+  executable = 'npm',
+  color
+) {
+  console.log(`\n\n ${color('正在安装项目依赖 ...')}`)
+  console.log('========================\n')
+  return runCommand(executable, ['install'], {
+    cwd,
+  })
+}
+
+
+function runCommand(cmd, args, options) {
+  return new Promise((resolve, reject) => {
+    /**
+     * 这里就是 在项目目录下执行 npm install
+     */
+    const spwan = spawn(
+      cmd,
+      args,
+      Object.assign(
+        {
+          cwd: process.cwd(),
+          stdio: 'inherit',
+          shell: true, // 在shell下执行
+        },
+        options
+      )
+    )
+    spwan.on('exit', () => {
+      resolve()
+    })
+  })
+}
